b361d624f54e317a6bd1018a07b8434aa9dc8a13
Marat Dospolov <marat.dospolov@gmail.com>
2020-08-12 23:15:12 +0300

    yt slider removed

--- build/snippets/youtube.js
+++ build/snippets/youtube.js
@@ -1,64 +1,7 @@
 ;(() => {
-  const generateSlider = (SPEEDS, SPEED_DEFAULT) => {
-    const slider = document.createElement('input')
-    slider.type = 'range'
-    slider.id = 'speed-dna'
-    slider.min = SPEEDS[0] * 100
-    slider.max = SPEEDS[SPEEDS.length - 1] * 100
-    slider.value = SPEED_DEFAULT * 100
-    slider.setAttribute('step', '25')
-    return slider
-  }
-
-  const generateDisplay = (SPEEDS, SPEED_DEFAULT) => {
-    const display = document.createElement('div')
-    display.id = 'speed-dna-display'
-    display.innerText = SPEED_DEFAULT
-    return display
-  }
-
   const snippet = {
     name: 'youtube',
     css: `
-      #speed-dna {
-        -webkit-appearance: none;
-        height: 36px;
-        border-radius: 1px;
-        background: transparent;
-        padding: 0;
-        width: 120px;
-        vertical-align: top;
-        outline: none;
-        cursor: pointer;
-        margin: 0 5px 0 6px;
-      }
-      
-      #speed-dna::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        width: 12px;
-        height: 12px;
-        border-radius: 50%;
-        background: #fff;
-        outline: none;
-        cursor: pointer;
-        margin-top: -4px;
-      }
-      
-      #speed-dna::-webkit-slider-runnable-track {
-        background: #ffffff55;
-        height: 3px;
-      }
-
-      #speed-dna-display {
-        font-size: 109%;
-        display: inline-block;
-        vertical-align: top;
-        padding: 0 5px;
-        white-space: nowrap;
-        line-height: 35px;
-      }
-
       .ytp-ce-covering-overlay,
       .ytp-ce-element-shadow,
       .ytp-ce-covering-image,
@@ -73,31 +16,7 @@
       #items .ytd-mini-guide-renderer:nth-child(2) { display: none; }
     `,
     js: elementReady => {
-      const SPEEDS_COUNT = 10
-      const SPEED_BASE = 0.25
-      const SPEEDS = [...Array(SPEEDS_COUNT)].map((_, i) => i * SPEED_BASE + 1)
-      const SPEED_DEFAULT = SPEEDS[1]
-
-      elementReady('video.html5-main-video', video => {
-        video.playbackRate = SPEED_DEFAULT
-        console.log(`youtube SPEED_DEFAULT ${SPEED_DEFAULT} applied`)
-
-        elementReady('.ytp-left-controls', container => {
-          if (!container.childElementCount) return
-
-          const slider = generateSlider(SPEEDS, SPEED_DEFAULT)
-          container.append(slider)
-
-          const display = generateDisplay(SPEEDS, SPEED_DEFAULT)
-          container.append(display)
-
-          slider.addEventListener('input', () => {
-            video.playbackRate = slider.value / 100
-            display.innerText = slider.value / 100
-          })
-          console.log('youtube speed slider added')
-        })
-      })
+      elementReady('ytd-live-chat-frame', chat => chat.remove())
     }
   }
 
